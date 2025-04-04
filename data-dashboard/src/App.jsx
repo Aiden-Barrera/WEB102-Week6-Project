61066f6bbb0255b5acca396f194dfd765c384f1c
Aiden Barrera <aidenbarrera@macbookpro.resource.campus.njit.edu>
2025-04-04 19:22:25 -0400

    Added Last statistic

--- data-dashboard/src/App.jsx
+++ data-dashboard/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [info, setInfo] = useState(null)
   const [totalClasses, setTotalClasses] = useState(0)
   const [totalSpells, setTotalSpells] = useState(0)
+  const [highestSpell, setHighestSpell] = useState(0)
 
   const fetchInfo = async () => {
     try{
@@ -15,7 +16,9 @@ function App() {
       setTotalClasses(res.data.count)
       const res1 = await axios.get("https://www.dnd5eapi.co/api/2014/spells")
       setTotalSpells(res1.data.count)
-      console.log(res.data)
+      const res2 = await axios.get("https://www.dnd5eapi.co/api/2014/classes/wizard/spells")
+      setHighestSpell(res2.data.count)
+      console.log(res2.data)
     } catch (err) {
       console.log("Error Fetching Data from DnD API:", err)
     }
@@ -27,7 +30,7 @@ function App() {
 
   return (
     <>
-      <div style={{display: "flex", justifyContent: 'space-evenly'}}>
+      <div style={{display: "flex", justifyContent: 'space-evenly', gap: "10px"}}>
         <div className='api-card' style={{margin: 0 }}>
           <h1>{totalClasses}</h1>
           <h2>Total No. of Classes</h2>
@@ -35,6 +38,10 @@ function App() {
         <div className='api-card' style={{margin: 0}}>
           <h1>{totalSpells}</h1>
           <h2>Total No. of Spells</h2>
+        </div>
+        <div className='api-card' style={{margin: 0}}>
+          <h1>{highestSpell}</h1>
+          <h2>Class: Wizard has the Most Spells</h2>
         </div>
         <div className='api-card' style={{margin: 0, padding: "0 2%"}}>
           <img src="/dnd.png" alt="dnd" style={{width: "225px", height: "auto", borderRadius: "16px"}} />
